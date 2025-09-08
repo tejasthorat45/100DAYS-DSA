4f8b3f443e24c1c3aa9f9eb7660847df0346aa38
Tejas Thorat <thorattejas809@gmail.com>
2025-09-08 14:18:14 +0530

    init commit

--- src/LeetCode100Days/Day7_Q11.java
+++ src/LeetCode100Days/Day7_Q11.java
@@ -1,6 +1,10 @@
 package LeetCode100Days;
 
 public class Day7_Q11 {
+    public static int hi(){
+        System.out.println("Hello Anmol");
+        return  0 ;
+    }
 
     public static void main(String[] args) {
         System.out.println("hello git");
