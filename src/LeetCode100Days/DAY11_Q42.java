f52d439ddb5f51d58d4ab6f3eb3bb6b970a92874
Tejas Thorat <thorattejas809@gmail.com>
2025-09-13 10:41:17 +0530

    git push origin main

--- src/LeetCode100Days/DAY11_Q42.java
+++ src/LeetCode100Days/DAY11_Q42.java
@@ -1,7 +1,39 @@
 package LeetCode100Days;
 //day3
 
-public class Trappedrainwater {
+public class DAY11_Q42 {
+
+    public static int bruteForce(int arr[]){
+
+        int n = arr.length;
+        int leftmax[] = new int[n];
+        leftmax[0]=arr[0];
+
+
+        //cal left amx
+
+        for(int i=1;i<n;i++){
+           leftmax[i] = Math.max(leftmax[i-1],arr[i] );
+
+        }
+        //cal rigth max
+        int rightmax[] = new int [n];
+        rightmax[n-1]=arr[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightmax[i] = Math.max(arr[i],rightmax[i+1]);
+        }
+        //cal water level
+
+       int trappedwater =0;
+        for(int i=0;i<n;i++){
+            int waterlevel = Math.min(leftmax[i],rightmax[i]);
+            trappedwater += waterlevel-arr[i];
+
+
+        }
+        return trappedwater;
+    }
 
     public static int trapeedwater(int arr[]){
         int left =0; int right =arr.length-1;
@@ -34,6 +66,7 @@ public class Trappedrainwater {
 
         int arr[]={4,2,0,5,3,2,4};
         System.out.println(trapeedwater(arr));
+        System.out.println(bruteForce(arr));
 
 
     }
